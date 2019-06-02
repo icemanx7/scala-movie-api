b93454b83a1b3535c91c8035ba82c91296540fd1
Richard Thomas <richard.thomas36@gmail.com>
2019-06-02 11:49:48 +0200

    removed http4s

--- build.sbt
+++ build.sbt
@@ -11,29 +11,9 @@ libraryDependencies ++= Seq(
   "org.xerial"          %  "sqlite-jdbc"          % "3.7.2"
 )
 
-val http4sVersion = "0.20.0-M6"
-
-// Only necessary for SNAPSHOT releases
-resolvers += Resolver.sonatypeRepo("snapshots")
-
 libraryDependencies ++= Seq(
-  "org.http4s" %% "http4s-dsl" % http4sVersion,
-  "org.http4s" %% "http4s-blaze-server" % http4sVersion,
-  "org.http4s" %% "http4s-blaze-client" % http4sVersion,
-  "org.http4s" %% "http4s-dsl" % http4sVersion,
-  "com.typesafe.slick" %% "slick" % "3.3.0"
-  
+  "com.typesafe.akka" %% "akka-http"   % "10.1.8",
+  "com.typesafe.akka" %% "akka-stream" % "2.5.19" // or whatever the latest version is
 )
 
-
-libraryDependencies ++= Seq(
-  "org.http4s" %% "http4s-circe" % http4sVersion,
-  // Optional for auto-derivation of JSON codecs
-  "io.circe" %% "circe-generic" % "0.11.1",
-  // Optional for string interpolation to JSON model
-  "io.circe" %% "circe-literal" % "0.11.1"
-)
-
-addCompilerPlugin("org.scalamacros" % "paradise" % "2.1.0" cross CrossVersion.full)
-
-scalacOptions ++= Seq("-Ypartial-unification")
+libraryDependencies += "com.typesafe.akka" %% "akka-http-spray-json" % "10.1.8"
--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,77 +1,8 @@
-import cats.effect._
-import cats.implicits._
-import org.http4s.HttpRoutes
-import org.http4s.syntax._
-import org.http4s.dsl.io._
-import org.http4s.implicits._
-import org.http4s.server.blaze._
+import models.MovieRepository
 
-import scala.concurrent.ExecutionContext.Implicits.global
-import slick.jdbc.SQLiteProfile.api._
-import io.circe.syntax._
-
-import scala.concurrent.Await
-import scala.concurrent.duration._
-import cats.effect._
-import io.circe._
-import io.circe.literal._
-import org.http4s._
-import org.http4s.dsl.io._
-import org.http4s.circe._
-import models.{Movie, MovieRepository}
-import cats.data.Kleisli
-import cats.effect._
-import cats.implicits._
-import org.http4s._
-import org.http4s.dsl.io._
-import org.http4s.implicits._
-import org.http4s.server.middleware.CORS
-
-
-
-class Main {
-
-}
-
-
-
-object Main extends IOApp {
+object Main {
 
   val dbInstance = new MovieRepository
 
-  implicit val HelloEncoder: Encoder[Movie] =
-    Encoder.instance { movie: Movie =>
-      json"""
-             {
-            "id": ${movie.id},
-            "title": ${movie.title},
-            "year": ${movie.year}
-             }"""
-    }
-
-  val helloWorldService = HttpRoutes.of[IO] {
-    case GET -> Root / "movies" =>
-      val dd = dbInstance.getAll
-      val result = Await.result(dd, 1.seconds)
-      Ok(result.asJson)
-
-    case GET -> Root / "movies" / id =>
-      val dd = dbInstance.findById(id)
-      val result = Await.result(dd, 1.seconds)
-      Ok(result.asJson)
-
-
-  }.orNotFound
-
-  val corsService = CORS(helloWorldService)
-
-  def run(args: List[String]): IO[ExitCode] =
-    BlazeServerBuilder[IO]
-      .bindHttp(8080, "localhost")
-      .withHttpApp(corsService)
-      .serve
-      .compile
-      .drain
-      .as(ExitCode.Success)
 
 }
