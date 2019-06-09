ff2c1f71405d57c757bed63630e47751ba8fb415
Richard Thomas <richard.thomas36@gmail.com>
2019-06-09 11:59:22 +0200

    added auth

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,19 +1,13 @@
 import java.time.Instant
-
 import models.{Movie, MovieRepository}
 import akka.actor.ActorSystem
 import akka.http.scaladsl.Http
 import akka.stream.ActorMaterializer
-import akka.Done
-import akka.http.scaladsl.server.Route
+import akka.http.scaladsl.server.{Directive1, Route}
 import akka.http.scaladsl.server.Directives._
 import akka.http.scaladsl.model.StatusCodes
 import akka.http.scaladsl.model.headers.RawHeader
-import pdi.jwt.{JwtAlgorithm, JwtClaim, JwtHeader, JwtSprayJson}
-
-import scala.util.{Failure, Success}
-// for JSON serialization/deserialization following dependency is required:
-// "com.typesafe.akka" %% "akka-http-spray-json" % "10.1.7"
+import pdi.jwt.{JwtAlgorithm, JwtClaim, JwtSprayJson}
 import akka.http.scaladsl.marshallers.sprayjson.SprayJsonSupport._
 import spray.json.DefaultJsonProtocol._
 
@@ -24,16 +18,18 @@ import scala.concurrent.Future
 final case class Movies(movies: List[Movie])
 final case class LoginRequest(username: String, password: String)
 
+
 object Main {
 
   val dbInstance = new MovieRepository
+
   private val tokenExpiryPeriodInDays = 1
-  private val secretKey               = "super_secret_key"
+  private val secretKey = "super_secret_key"
   val algo = JwtAlgorithm.HS256
   val claim = JwtClaim(
-         expiration = Some(Instant.now.plusSeconds(157784760).getEpochSecond),
-         issuedAt = Some(Instant.now.getEpochSecond)
-       )
+    expiration = Some(Instant.now.plusSeconds(157784760).getEpochSecond),
+    issuedAt = Some(Instant.now.getEpochSecond)
+  )
   val token = JwtSprayJson.encode(claim, secretKey, algo)
 
   implicit val system = ActorSystem()
@@ -46,30 +42,51 @@ object Main {
   implicit val moviesFormat = jsonFormat1(Movies)
   implicit val loginRequest = jsonFormat2(LoginRequest)
 
+  private def authenticated: Directive1[String] =
+    optionalHeaderValueByName("Authorization").flatMap {
+      case Some(jwt) if JwtSprayJson.isValid(jwt) =>
+        complete(StatusCodes.Unauthorized -> "Token expired.")
+
+      case Some(jwt) if JwtSprayJson.isValid(jwt, secretKey, Seq(JwtAlgorithm.HS256)) =>
+        provide(JwtSprayJson.decode(jwt).toString)
+
+      case _ => complete(StatusCodes.Unauthorized)
+    }
+
+
+  private def getListMovies: Route = get {
+    authenticated { claims  =>
+      pathPrefix("movies" ) {
+        val maybeItem:Future[List[Movie]] = dbInstance.getAll
+        print(token)
+        print(claims)
+
+        onSuccess(maybeItem) {
+          case res => complete(res)
+          case _ => complete(StatusCodes.NotFound)
+        }
+      }
+    }
+  }
+
+  private def login: Route = post {
+    entity(as[LoginRequest]) {
+      case LoginRequest("admin", "admin") =>
+        respondWithHeader(RawHeader("Access-Token", token)) {
+          complete(StatusCodes.OK)
+        }
+      case LoginRequest(_, _) => complete(StatusCodes.Unauthorized)
+    }
+  }
+
+  private def getSingleMovie: Route = ???
+
+  private def findMovies: Route = ???
+
+
   def main(args: Array[String]) {
 
-    val route: Route =
-      get {
-        pathPrefix("movies" ) {
-          val maybeItem:Future[List[Movie]] = dbInstance.getAll
-          print(token)
-
-           onSuccess(maybeItem) {
-            case res => complete(res)
-            case _ => complete(StatusCodes.NotFound)
-          }
-
-        }
-      } ~
-        post {
-          entity(as[LoginRequest]) {
-            case lr@LoginRequest("admin", "admin") =>
-              respondWithHeader(RawHeader("Access-Token", token)) {
-                complete(StatusCodes.OK)
-              }
-            case LoginRequest(_, _) => complete(StatusCodes.Unauthorized)
-          }
-        }
+    val route: Route = getListMovies ~ login
 
     val bindingFuture = Http().bindAndHandle(route, "localhost", 8080)
     println(s"Server online at http://localhost:8080/\nPress RETURN to stop...")
