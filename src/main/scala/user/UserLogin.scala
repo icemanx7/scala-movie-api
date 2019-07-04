3fb7b61b7b6417b71b09904a599cd05b5145b43c
Richard Thomas <richard.thomas36@gmail.com>
2019-07-04 18:37:08 +0200

    add user login service

--- src/main/scala/user/UserLogin.scala
+++ src/main/scala/user/UserLogin.scala
@@ -7,7 +7,6 @@ import akka.http.scaladsl.server.Directives._
 import akka.http.scaladsl.model.StatusCodes
 import akka.http.scaladsl.marshallers.sprayjson.SprayJsonSupport._
 import akka.http.scaladsl.model.headers.RawHeader
-import movies.MovieRepository
 import utils.MarshallFormatImplicits._
 
 class UserLogin(userService: UserService) {
@@ -15,8 +14,8 @@ class UserLogin(userService: UserService) {
   // Add the userService That will check the credentials.
   def login: Route = post {
     entity(as[LoginRequest]) { user =>
-      val isIndb = userService.login(user)
-      onSuccess(isIndb) {
+      val isIndatabase = userService.login(user)
+      onSuccess(isIndatabase) {
         case Some(realUser) =>
           respondWithHeader(RawHeader("Access-Token", jwtToken.getToken)) {
             complete(realUser)
--- src/main/scala/user/UserService.scala
+++ src/main/scala/user/UserService.scala
@@ -2,20 +2,17 @@ package user
 
 import models.{LoggedInUser, LoginRequest}
 import scala.concurrent.{ExecutionContext, Future}
+import utils.MonadTransformers._
 
 class UserService(dbInstance: UserRepository)
                  (implicit executionContext: ExecutionContext) {
 
   def login(loginUser: LoginRequest): Future[Option[LoggedInUser]] = {
     dbInstance.findByUsername(loginUser.username)
-      .filter(user => {
-        user match  {
-          case Some(realUser) => "1" == "1"
-          case None => false
-        }
-      }
-      )
-      .map(user => user.map(realUser => LoggedInUser(realUser.id, realUser.name)))
+      .filterT(user => {
+        println(user)
+        user.password == loginUser.password})
+      .mapT(user => LoggedInUser(user.id, user.name))
   }
 
 }
