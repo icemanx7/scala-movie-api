3e5e98e0eb68653d8b120e973b989dccad06b62b
richard.thomas <richard.thomas36@gmail.com>
2020-07-01 17:01:16 +0200

    clean up

--- src/test/scala/user/LoginRouteSpec.scala
+++ src/test/scala/user/LoginRouteSpec.scala
@@ -1,16 +1,12 @@
 package user
 
 import akka.http.scaladsl.model.StatusCodes
-import akka.http.scaladsl.server.Directives._
-import akka.http.scaladsl.server._
 import akka.http.scaladsl.testkit.ScalatestRouteTest
 import authentication.JWTGenerator
 import models.{LoggedInUser, LoginRequest}
-import movies.{MovieRepository, MovieRoute, MovieService}
 import org.scalatest.matchers.should.Matchers
 import org.scalatest.wordspec.AnyWordSpec
 import pdi.jwt.{JwtAlgorithm, JwtSprayJson}
-import reviews.ReviewsRepository
 import utils.MarshallFormatImplicits
 
 class LoginRouteSpec extends AnyWordSpec with Matchers with ScalatestRouteTest with MarshallFormatImplicits  {
@@ -29,14 +25,13 @@ class LoginRouteSpec extends AnyWordSpec with Matchers with ScalatestRouteTest w
     "return 200 and valid token after successful login" in {
       Post("/login", loginUser) ~> userRoute.login ~> check {
         status shouldEqual StatusCodes.OK
-        val resp = responseAs[LoggedInUser]
-        val jwt = JwtSprayJson.isValid(resp.jwtToken, jwtToken.secretKey, Seq(JwtAlgorithm.HS256))
-        jwt shouldEqual true
+        val loginResponse = responseAs[LoggedInUser]
+        val isValidJWT = JwtSprayJson.isValid(loginResponse.jwtToken, jwtToken.secretKey, Seq(JwtAlgorithm.HS256))
+        isValidJWT shouldEqual true
       }
     }
 
     "return 401 after unsuccessful login" in {
-      // tests:
       Post("/login", wrongLoginUser) ~> userRoute.login ~> check {
         status shouldEqual StatusCodes.Unauthorized
       }
