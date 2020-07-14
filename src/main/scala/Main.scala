76177da4940d400ffd5e06db82bcb4c97f6f8041
richard.thomas <richard.thomas36@gmail.com>
2020-07-14 12:26:13 +0200

    added procfile

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -30,10 +30,10 @@ object Main {
     val route: Route = userRoute.login ~ movieRoute.getMovieEntities ~ movieRoute.submitMovieReview
 
     val bindingFuture = Http().bindAndHandle(route, "0.0.0.0", 8080)
-    println(s"Server online at http://localhost:8080/\nPress RETURN to stop...")
+//    println(s"Server online at http://localhost:8080/\nPress RETURN to stop...")
 //    StdIn.readLine()
     bindingFuture
-      .flatMap(_.unbind())
-      .onComplete(_ ⇒ system.terminate())
+//      .flatMap(_.unbind())
+//      .onComplete(_ ⇒ system.terminate())
   }
 }
